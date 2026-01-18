c526a8b9b83d532c13361b45c52300fa34d81c99
elliot <elliotanbz@gmail.com>
2026-01-19 01:05:09 +0800

    Add echo

--- src/main/java/Bossa.java
+++ src/main/java/Bossa.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
 public class Bossa {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
         System.out.println("Hello! I'm Bossa");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
 
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }

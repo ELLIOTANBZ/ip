bb3c9e093e9b8642048812eff8250429d2ebf31d
elliot <elliotanbz@gmail.com>
2026-01-19 00:35:00 +0800

    Add greeting

---
+++ src/main/java/Bossa.java
@@ -0,0 +1,8 @@
+public class Bossa {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Bossa");
+        System.out.println("What can I do for you?");
+
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
